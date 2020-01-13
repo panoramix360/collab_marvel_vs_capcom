14ddee7df17ad330aaa045d44bd2f9db43fd7404
Lucas Reis <lucas.oreis@tvglobo.com.br>
2020-01-13 19:37:17 -0300

    inserindo assets

---
+++ StreetFighterArtSelection/Assets/Scripts/SlidingBackground.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidingBackground : MonoBehaviour
+{
+    public float scrollSpeed;
+
+    private Vector2 savedOffset;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        float y = Mathf.Repeat(Time.time * scrollSpeed, 1);
+        Vector2 offset = new Vector2(y, 0);
+        transform.position = offset;
+    }
+}
