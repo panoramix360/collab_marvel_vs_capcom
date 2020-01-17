c979253f34634bcd60062bdecb11a3ca1b0ea762
Lucas Reis <lucasreis3000@gmail.com>
2020-01-17 00:49:07 -0300

    background moving

--- StreetFighterArtSelection/Assets/Scripts/SlidingBackground.cs
+++ StreetFighterArtSelection/Assets/Scripts/SlidingBackground.cs
@@ -1,21 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SlidingBackground : MonoBehaviour
 {
     public float scrollSpeed;
 
-    private Vector2 savedOffset;
+    private Image image;
 
     private void Start()
     {
+        image = GetComponent<Image>();
     }
 
     private void Update()
     {
-        float y = Mathf.Repeat(Time.time * scrollSpeed, 1);
-        Vector2 offset = new Vector2(y, 0);
-        transform.position = offset;
+        image.material.mainTextureOffset = new Vector2(0f, Time.time * scrollSpeed);
     }
 }
