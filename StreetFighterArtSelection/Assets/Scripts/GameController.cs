c48e58fa719b1bc5f03ed4c68c36f23dd1f1893a
Lucas Reis <lucasreis3000@gmail.com>
2020-01-19 15:11:12 -0300

    Inserindo a interação

---
+++ StreetFighterArtSelection/Assets/Scripts/GameController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : Singleton<GameController>
+{
+    public Image characterImage;
+    public Text characterName;
+    public Text artistInstagram;
+
+    public GameObject audio;
+    public AudioClip[] musics;
+
+    private Mini characterSelected;
+
+    public void SetCharacterSelected(Mini characterToSelect)
+    {
+        if (characterSelected != null)
+        {
+            if (characterSelected.characterName == characterToSelect.characterName)
+            {
+                return;
+            }
+            characterSelected.PlayIdle();
+        }
+
+        characterSelected = characterToSelect;
+
+        characterImage.sprite = characterSelected.image.GetComponent<Image>().sprite;
+        characterName.text = characterSelected.characterName;
+        artistInstagram.text = characterSelected.artistInstagram;
+    }
+
+    public void NextMusic()
+    {
+
+    }
+}
---
+++ StreetFighterArtSelection/Assets/Scripts/Mini.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Mini : MonoBehaviour
+{
+    public string characterName;
+    public string artistInstagram;
+    public GameObject image;
+    public GameObject container;
+
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = container.GetComponent<Animator>();
+    }
+
+    public void OnClick()
+    {
+        Debug.Log("OnClick: " + characterName);
+        animator.SetBool("play", true);
+        GameController.Instance.SetCharacterSelected(this);
+    }
+
+    public void PlayIdle()
+    {
+        animator.SetBool("play", false);
+    }
+}
